60542271fb540ccdb0a6795ac8b2ffa01d750fb4
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-04-24 13:52:28 +0300

    add front to project

---
+++ LearningCiCDClient/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { DataComponent } from './retrieving/data/data.component';
+
+export const routes: Routes = [
+    { path: 'display-data', component: DataComponent}
+];
