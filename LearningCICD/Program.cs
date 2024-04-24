66dd322675da594c211b09eca9f993906ca92213
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-04-24 13:51:38 +0300

    add cors

--- LearningCICD/Program.cs
+++ LearningCICD/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -12,7 +13,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(opts =>
+{
+    opts.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
 app.MapGet("/", () => $"This is {builder.Configuration["Environment"]} environment/maybe i should say fuck off?");
 
 app.Run();
