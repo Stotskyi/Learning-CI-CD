2b83b7a52a04e92bb8807bab9ab572f3ae87726b
Stotskyi <116818493+Stotskyi@users.noreply.github.com>
2024-04-24 19:03:48 +0300

    Update Program.cs

--- LearningCICD/Program.cs
+++ LearningCICD/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseCors(opts =>
 {
-    opts.WithOrigins("http://localhost:4200")
+    opts.WithOrigins("https://learning-ci-cd-client-2024.azurewebsites.net/")
         .AllowAnyMethod()
         .AllowAnyHeader();
 });
